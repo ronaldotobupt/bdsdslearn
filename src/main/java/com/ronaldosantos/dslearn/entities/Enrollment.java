1cba54ab40d1b9327fafc1b5b296050e48024be3
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-04-07 19:33:37 -0300

    Enrollment and EnrollmentPK

---
+++ src/main/java/com/ronaldosantos/dslearn/entities/Enrollment.java
@@ -0,0 +1,89 @@
+package com.ronaldosantos.dslearn.entities;
+
+import java.time.Instant;
+
+import com.ronaldosantos.dslearn.entities.pk.EnrollmentPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+	
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	private Instant enrollMoment;
+	private Instant refundMoment;
+	private boolean avaliable;
+	private boolean onlyUpdate;
+	
+	
+	public Enrollment() {
+		
+	}
+
+	public Enrollment(Instant enrollMoment, Instant refundMoment, boolean avaliable, boolean onlyUpdate,
+			User user, Offer offer) {
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.avaliable = avaliable;
+		this.onlyUpdate = onlyUpdate;
+		
+	}
+
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudente(User user) {
+		id.setUser(user);
+	}
+	
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvaliable() {
+		return avaliable;
+	}
+
+	public void setAvaliable(boolean avaliable) {
+		this.avaliable = avaliable;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+	
+	
+	
+
+}
