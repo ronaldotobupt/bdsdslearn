06fc43f0c1b4d85c86a85027bac854c3a1732139
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-04-07 16:51:50 -0300

    Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,3 +15,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_Uri,img_Gray_Uri) VALUES ('Bootcamp HTML','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
